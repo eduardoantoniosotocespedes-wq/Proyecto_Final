654ba99f15542aa63bf49dbdca9acda90d79a1a5
EduardoSoto <eduardoantoniosotocespede@gmail.com>
2026-05-24 14:59:18 -0400

    tercer commit

--- CreateAcount_login/create_acount_login.py
+++ CreateAcount_login/create_acount_login.py
@@ -2,23 +2,36 @@ print("////SISTEMA DE CONTROL DE VISITAS MEDICAS///")
 employee  =[]
 password =[]
 while True:
-    print("-Eliga una una de las opciones.-")
+    print("--Eliga una una de las opciones.--")
     print("*1.-Crear cuenta.")
     print("*2.-Ingresar.")
-    option = int(input("responda aqui: "))
+    print("*3.-Salir. ")
+    option = int(input("responda aqui: "))#error si se poone un especio vacio  letras
     if option == 1:
-        user =str(input("ingrese su nombre de usuario: "))
-        clave =str(input("ingrese su contraseña"))
-        if user  not in employee:
+        user = str(input("///Crea tu nombre de usuario: "))
+        clave = str(input("///Crea tu contraseña contraseña: "))
+        if user not in employee:
             employee.append(user)
             password.append(clave)
+            print("///Cuenta creada con exito.")
+            print(employee )
+            print(password)
         else:
-            print("este usuario ya existe ingrese otro")    
-    if option == 1:
-        user =str(input("ingrese su nombre de usuario: "))
-        clave =str(input("ingrese su contraseña"))
-        if user in employee and clave in password:
-            print("bienvenido al sistema")
+            print("///Este usuario ya existe ingrese otro.")    
+    elif option == 2:
+        user =str(input("///Ingrese su nombre de usuario: "))
+        clave =str(input("///Ingrese su contraseña: "))
+        if user in employee:
+            lista = employee.index(user)
+            if password[lista] == clave:
+                print("///Bienvenido al sistema.")
+                break #aqui pondre el menu iteractivo despues de pasar el login o creaccion de cuenata
+            else:
+                print("///Usuario o contraseña incorrecta.") 
         else:
-            print("usuario o contraseña incorrecta")    
-            break
+            print("///Este usuario no existe.")           
+    elif option == 3:
+        print("///Saliendo...")
+        break
+    else:
+        print("///Opcion no valida, intente denuevo.")       
