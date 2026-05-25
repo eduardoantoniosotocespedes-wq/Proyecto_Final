15dece7b6d91675625addfecc97931b05f735357
EduardoSoto <eduardoantoniosotocespede@gmail.com>
2026-05-25 00:57:47 -0400

    tercer commit

--- CreateAcount_login/create_acount_login.py
+++ CreateAcount_login/create_acount_login.py
@@ -5,33 +5,37 @@ while True:
     print("--Eliga una una de las opciones.--")
     print("*1.-Crear cuenta.")
     print("*2.-Ingresar.")
-    print("*3.-Salir. ")
-    option = int(input("responda aqui: "))#error si se poone un especio vacio  letras
-    if option == 1:
-        user = str(input("///Crea tu nombre de usuario: "))
-        clave = str(input("///Crea tu contraseña contraseña: "))
-        if user not in employee:
-            employee.append(user)
-            password.append(clave)
-            print("///Cuenta creada con exito.")
-            print(employee )
-            print(password)
-        else:
-            print("///Este usuario ya existe ingrese otro.")    
-    elif option == 2:
-        user =str(input("///Ingrese su nombre de usuario: "))
-        clave =str(input("///Ingrese su contraseña: "))
-        if user in employee:
-            lista = employee.index(user)
-            if password[lista] == clave:
-                print("///Bienvenido al sistema.")
-                break #aqui pondre el menu iteractivo despues de pasar el login o creaccion de cuenata
+    print("///para salir escriba (SALIR).")
+    initial =(input("responda aqui: "))#error si se poone un especio vacio  letras
+    if initial.isdigit(): #usando este metodo para separar valorres str i int
+        option = int(initial)
+        if option == 1:#condicional para crear cuenta
+            user = str(input("///Crea tu nombre de usuario: "))
+            clave = str(input("///Crea tu contraseña contraseña: "))
+            if user not in employee:
+                employee.append(user)#metodos para agregar variables a las listas
+                password.append(clave)
+                print("///Cuenta creada con exito.")
             else:
-                print("///Usuario o contraseña incorrecta.") 
+                print("///Este usuario ya existe ingrese otro.")    
+        elif option == 2: #condicional para ingresar 
+            user =str(input("///Ingrese su nombre de usuario: "))
+            clave =str(input("///Ingrese su contraseña: "))
+            if user in employee:
+                user_1 = employee.index(user)#metodo para buscar su pocicion en la lista 
+                if password[user_1] == clave:#comparacion de pociciones en laslistas
+                    print("///Bienvenido al sistema.")
+                    break #aqui pondre el menu iteractivo despues de pasar el login o creaccion de cuenata
+                else:
+                    print("///Usuario o contraseña incorrecta.") 
+            else:
+                print("///Este usuario no existe.")           
         else:
-            print("///Este usuario no existe.")           
-    elif option == 3:
-        print("///Saliendo...")
-        break
+            print("///Opcion no valida, intente denuevo.")       
     else:
-        print("///Opcion no valida, intente denuevo.")       
+        option = str(initial)#opcion de cerrar el progrrma con la palabra pedida en la diapositiva
+        if option == "SALIR":
+            print("///saliendo...")
+            break
+        else:
+            print("///Opcion desconocida, intente denuevo")    
