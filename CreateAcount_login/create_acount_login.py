d9faa72209cd5feda2ce9f200628ddee136ddddb
EduardoSoto <eduardoantoniosotocespedes@gmail.com>
2026-05-27 08:29:18 -0400

    tercer commit

--- CreateAcount_login/create_acount_login.py
+++ CreateAcount_login/create_acount_login.py
@@ -6,12 +6,12 @@ while True:
     print("*1.-Crear cuenta.")
     print("*2.-Ingresar.")
     print("///para salir escriba (SALIR).")
-    initial =(input("responda aqui: "))#error si se poone un especio vacio  letras
+    initial =(input("responda aqui: ")).upper().strip()#error si se poone un especio vacio  letras
     if initial.isdigit(): #usando este metodo para separar valorres str i int
         option = int(initial)
         if option == 1:#condicional para crear cuenta
-            user = str(input("///Crea tu nombre de usuario: "))
-            clave = str(input("///Crea tu contraseña contraseña: "))
+            user = str(input("///Crea tu nombre de usuario: ")).lower().strip()
+            clave = str(input("///Crea tu contraseña contraseña: ")).lower().strip()
             if user not in employee:
                 employee.append(user)#metodos para agregar variables a las listas
                 password.append(clave)
@@ -19,8 +19,8 @@ while True:
             else:
                 print("///Este usuario ya existe ingrese otro.")    
         elif option == 2: #condicional para ingresar 
-            user =str(input("///Ingrese su nombre de usuario: "))
-            clave =str(input("///Ingrese su contraseña: "))
+            user =str(input("///Ingrese su nombre de usuario: ")).lower().strip()
+            clave =str(input("///Ingrese su contraseña: ")).lower().strip()
             if user in employee:
                 user_1 = employee.index(user)#metodo para buscar su pocicion en la lista 
                 if password[user_1] == clave:#comparacion de pociciones en laslistas
